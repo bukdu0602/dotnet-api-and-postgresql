7b868c962a6d8ebf40419d073d0002b7615c7a6d
Ryan Lim <ryan.hyun@gmail.com>
2022-04-17 19:50:35 -0700

    initial

---
+++ kitimat2/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kitimat2.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
